643f8f3814c58f1491e120cdc3818361ee01cc81
Dimitri Almon <dimitri.almon@gmail.com>
2024-07-08 16:27:51 +0200

    adding threejs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
+
 class PagesController < ApplicationController
   def home
+  end
+
+  def hero_pre_home
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,4 @@
-# Pin npm packages by running ./bin/importmap
-
+# config/importmap.rb
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
@@ -7,3 +6,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+
+# Ajout de three.js et GLTFLoader depuis npm
+pin "three", to: "three/build/three.module.js"
+pin "GLTFLoader", to: "three/examples/jsm/loaders/GLTFLoader.js"
